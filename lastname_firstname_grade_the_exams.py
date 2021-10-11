aaa40eee7377129fa1abd9df53769667a753b661
lyphatvietlinh <lyphatvietlinh@gmail.com>
2021-10-11 15:04:45 +0700

    Adding Task 3

--- lastname_firstname_grade_the_exams.py
+++ lastname_firstname_grade_the_exams.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 # TASK 1
 def checkfile(filename):
     """
@@ -17,8 +20,6 @@ def checkfile(filename):
 
 
 # TASK 2
-# This is some changes
-# This is some changes that was made on the remote repo. Pull before doing local work.
 def analyze(filename):
     """
     Analyze the file and detect invalid lines before doing other works.
@@ -30,21 +31,49 @@ def analyze(filename):
     with open(filename, 'r') as file_object:
         total_lines = 0
         valid_lines = 0
-        for i, line in enumerate(file_object):
-            result = file_object.readline().strip().split(",")
+        assignments_dict = dict()
+        for line in file_object:
+            result = line.strip().split(",")
             if len(result) != 26:
-                print("Error found at line {}".format(i))
-                print(result)
+                print("Not enough answers from student: {}".format(result[0]))
             else:
                 valid_lines += 1
+                assignments_dict[result[0]] = result[1:]
             total_lines += 1
         if valid_lines == total_lines:
             print("No errors found!")
         print("REPORT!".center(30, "-"))
+        print("Total lines of data: {}".format(total_lines))
         print("Total valid lines of data: {}".format(valid_lines))
         print("Total invalid lines of data: {}".format(total_lines - valid_lines))
+    return assignments_dict
+
+
+# TASK 3
+def grade(assignments_dict):
+    print("GRADING!".center(30, "-"))
+    answer_key = "B,A,D,D,C,B,D,A,C,C,D,B,A,B,A,C,B,D,A,C,A,A,B,D,D".strip().split(",")
+    for student, result in assignments_dict.items():
+        score = 0
+        for i in range(len(result)):
+            if result[i] == answer_key[i]:
+                score += 4
+            elif result[i] == "":
+                pass
+            else:
+                score += -1
+        assignments_dict[student] = score
+    score = np.array(list(assignments_dict.values()))
+    print("{} students graded!".format(len(assignments_dict)))
+    print("Average score: {}".format(score.mean()))
+    print("Highest score: {}".format(score.max()))
+    print("Lowest score: {}".format(score.min()))
+    print("Range of scores: {}".format(score.max() - score.min()))
+    print("Median value: {}".format(np.median(score)))
+    return assignments_dict
 
 
 file = input("Input your filename: ")
 if checkfile(file):
-    analyze(file)
+    assignments = analyze(file)
+    scores = grade(assignments)
