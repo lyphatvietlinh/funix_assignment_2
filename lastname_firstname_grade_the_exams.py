cb16fb4b9180d10ba8adabb5da3afcd05c28bdba
lyphatvietlinh <lyphatvietlinh@gmail.com>
2021-10-11 13:42:59 +0700

    minor changes

--- lastname_firstname_grade_the_exams.py
+++ lastname_firstname_grade_the_exams.py
@@ -30,10 +30,13 @@ def analyze(filename):
         for i, line in enumerate(file_object):
             result = file_object.readline().strip().split(",")
             if len(result) != 26:
-                print("Error found at line {} {}".format(i, result))
+                print("Error found at line {}".format(i))
+                print(result)
             else:
                 valid_lines += 1
             total_lines += 1
+        if valid_lines == total_lines:
+            print("No errors found!")
         print("REPORT!".center(30, "-"))
         print("Total valid lines of data: {}".format(valid_lines))
         print("Total invalid lines of data: {}".format(total_lines - valid_lines))
