882ccf45f7cab82672819139d4afdbd12fe892d5
lyphatvietlinh <lyphatvietlinh@gmail.com>
2021-10-12 08:59:15 +0700

    add documentation

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# Introduction to Data Science - Assignment 2
-This is a project for doing FUNIX assignment. 
+# Introduction
+This is a simple project for automated student's test answers, and output the scores in a text file.
+# Instruction
+When run, the program will ask for a filename input, to start grading. The input file should be stored in the same directory with the source code file.
+
+The input file for grading should be in text file format (.txt), with each line represents one student's answers.
+```
+N12345678,B,A,D,D,C,B,D,A,C,C,D,B,A,B,A,C,B,D,A,C,A,A,B,D,D
+N00000021,B,A,C,D,C,C,D,D,C,C,D,B,,B,A,C,B,,A,D,A,A,B,D,
+N00000022,B,A,D,A,C,B,D,D,C,C,D,B,A,B,A,,B,A,A,C,A,A,B,A,D
+N00000023,,A,D,D,C,B,D,A,C,C,,C,,B,A,C,B,D,A,C,A,A
+N00000024,C,C,D,D,C,B,,A,C,C,D,B,A,B,,C,B,D,A,C,A,B,B,,D
+```
+The graded scores will be saved in an output text file (.txt), with the file name changed to "original_filename_grades.txt", in the same directory. Each line contains one student's code and his score.
+```
+N00000001,59
+N00000002,70
+N00000003,84
+N00000004,73
+N00000005,83
+```
+# Data Validation
+1. The program will first attempt to find and open the file. An error is output if the file cannot be found. 
+
+2. If the file can be found, then it will continue to open the file for data validation: 
+   * The first value should be the student code. It should contain the character “N” followed by 8 numeric characters
+   * The following number of values should be corresponding to the number of answers, which in this case is 25 answers. 
+   * Line with invalid data will be printed out. 
+   * A report of total lines including valid and invalid lines will  be printed out
+
+3. Only valid data will be graded and saved into the output file. 
+
+# Grading
+Grading will be followed by these policies:
+* +4 points for every right answer
+* 0 points for every skipped answer
+* -1 point for every incorrect answer
+
+Statistics for the file will be printed out (but not saved in the output file):
+* Average score
+* Highest score
+* Lowest score
+* Score range
+* Median value
--- linh_ly_grade_the_exams.py
+++ linh_ly_grade_the_exams.py
@@ -3,145 +3,162 @@
 # --------------------------------------------------------------------
 import numpy as np
 
+answer_key = "B,A,D,D,C,B,D,A,C,C,D,B,A,B,A,C,B,D,A,C,A,A,B,D,D" \
+	.strip().split(",")
+no_of_answers = len(answer_key)
+
 
 # TASK 1
 def check_file(filename: str):
-    """
+	"""
     Checks if the file exists before doing further works.
 
     :param filename: file to check
     :return: true/false on file existence
     """
-    print("\n" + "FILE CHECKING".center(30, "-") + "\n")
-    try:
-        open(filename)
-        print("File '{}' founded!".format(filename))
-        return True
-    except FileNotFoundError:
-        print("Could not find file '{}'!".format(filename))
-        return False
+	print("\n" + "FILE CHECKING".center(30, "-") + "\n")
+	try:
+		open(filename)
+		print("File '{}' founded!".format(filename))
+		return True
+	except FileNotFoundError:
+		print("Could not find file '{}'!".format(filename))
+		return False
 
 
 # TASK 2
 def check_student_code(code: str):
-    """
+	"""
     To check if a student number/code is valid
 
     :param code: student code to check
     :return: true if the code is valid, false if not
     """
-    if code[0] != "N":
-        return False
-    if len(code) != 9:
-        return False
-    for char in code[1:]:
-        if not char.isdigit():
-            return False
-    return True
+	if code[0] != "N":
+		return False
+	if len(code) != 9:
+		return False
+	for char in code[1:]:
+		if not char.isdigit():
+			return False
+	return True
 
 
 def analyze(filename: str):
-    """
+	"""
     Analyze the file and detect invalid lines before doing other works.
 
     :param filename: file to check
-    :return: dictionary of student_code:graded_score
+    :return: dictionary of student_code:graded_score. Return 0 if every
+    line is invalid
     """
-    print("\n" + "ANALYSING!".center(30, "-") + "\n")
-    with open(filename, 'r') as file_object:
-        total_lines = 0
-        valid_lines = 0
-        assignments_dict = dict()
-        for line in file_object:
-            result = line.strip().split(",")
-            if len(result) != 26:
-                print("Invalid data - Incorrect number of answers ({})"
-                      .format(len(result)-1))
-                print(result)
-            elif not check_student_code(result[0]):
-                print("Invalid data - Incorrect student code")
-                print(result)
-            else:
-                valid_lines += 1
-                assignments_dict[result[0]] = result[1:]
-            total_lines += 1
-        if valid_lines == total_lines:
-            print("No errors found!")
-        print("\n" + "REPORTING!".center(30, "-") + "\n")
-        print("Total lines of data:".ljust(30, "-") + " {}"
-              .format(total_lines))
-        print("Total valid lines of data:".ljust(30, "-") + " {}"
-              .format(valid_lines))
-        print("Total invalid lines of data:".ljust(30, "-") + " {}"
-              .format(total_lines - valid_lines))
-    return assignments_dict
+	print("\n" + "ANALYSING!".center(30, "-") + "\n")
+	with open(filename, 'r') as file_object:
+		total_lines = 0
+		valid_lines = 0
+		assignments_dict = dict()
+
+		# Check and validate every line in file for: number of answers,
+		# and student code. Store valid data in a dict with key:value pair
+		# as student:answers
+		for line in file_object:
+			result = line.strip().split(",")
+			if len(result) - 1 != no_of_answers:
+				print("Invalid data - Incorrect number of answers ({})"
+					  .format(len(result) - 1))
+				print(result)
+			elif not check_student_code(result[0]):
+				print("Invalid data - Incorrect student code")
+				print(result)
+			else:
+				valid_lines += 1
+				assignments_dict[result[0]] = result[1:]
+			total_lines += 1
+
+		if valid_lines == total_lines:
+			print("No errors found!")
+
+		print("\n" + "REPORTING!".center(30, "-") + "\n")
+		print("Total lines of data:".ljust(30, "-") + " {}"
+			  .format(total_lines))
+		print("Total valid lines of data:".ljust(30, "-") + " {}"
+			  .format(valid_lines))
+		print("Total invalid lines of data:".ljust(30, "-") + " {}"
+			  .format(total_lines - valid_lines))
+
+		if valid_lines == 0:
+			return 0
+
+	return assignments_dict
 
 
 # TASK 3
 def grade(assignments_dict: dict):
-    """
+	"""
     Grade the input assignment dictionary
 
     :param assignments_dict: dict type input, with key = student code,
     value = answers
     :return: a dict contains key = student code, value = score
     """
-    print("\n" + "GRADING!".center(30, "-") + "\n")
-    answer_key = "B,A,D,D,C,B,D,A,C,C,D,B,A,B,A,C,B,D,A,C,A,A,B,D,D"\
-        .strip().split(",")
-    for student, result in assignments_dict.items():
-        score = 0
-        for i in range(len(result)):
-            if result[i] == answer_key[i]:
-                score += 4
-            elif result[i] == "":
-                pass
-            else:
-                score += -1
-        assignments_dict[student] = score
-    score = np.array(list(assignments_dict.values()))
-    print("Number of students graded:".ljust(30, "-")
-          + " {}".format(len(assignments_dict)))
-    print("Average score:".ljust(30, "-")
-          + " {:.2f}".format(score.mean()))
-    print("Highest score:".ljust(30, "-")
-          + " {}".format(score.max()))
-    print("Lowest score:".ljust(30, "-")
-          + " {}".format(score.min()))
-    print("Range of scores:".ljust(30, "-")
-          + " {}".format(score.max() - score.min()))
-    print("Median value:".ljust(30, "-")
-          + " {:.2f}".format(np.median(score)))
-    return assignments_dict
+	print("\n" + "GRADING!".center(30, "-") + "\n")
+
+	# Loop through every student:answers pair in the dict, compare with
+	# answer_key for grading, then replace the answers in the dict with
+	# the graded score
+	for student, result in assignments_dict.items():
+		score = 0
+		for i in range(len(result)):
+			if result[i] == answer_key[i]:
+				score += 4
+			elif result[i] == "":
+				pass
+			else:
+				score += -1
+		assignments_dict[student] = score
+
+	# Outputting statistic report
+	score = np.array(list(assignments_dict.values()))
+	print("Number of students graded:".ljust(30, "-")
+		  + " {}".format(len(assignments_dict)))
+	print("Average score:".ljust(30, "-")
+		  + " {:.2f}".format(score.mean()))
+	print("Highest score:".ljust(30, "-")
+		  + " {}".format(score.max()))
+	print("Lowest score:".ljust(30, "-")
+		  + " {}".format(score.min()))
+	print("Range of scores:".ljust(30, "-")
+		  + " {}".format(score.max() - score.min()))
+	print("Median value:".ljust(30, "-")
+		  + " {:.2f}".format(np.median(score)))
+
+	# Return the dict with student:score pairs
+	return assignments_dict
 
 
 # TASK 4
 def write_file(scores_dict: dict, filename: str):
-    """
+	"""
     Writes result scores into a file
 
     :param scores_dict: dict contains student code and score as key:value
     :param filename: original name of the file that was used for grading
     """
-    print("\n" + "EXPORTING RESULTS!".center(30, "-") + "\n")
-    filename = filename.replace(".txt", "_grades.txt")
-    with open(filename, "w+") as file_object:
-        for key, value in scores_dict.items():
-            file_object.write(key + "," + str(value) + "\n")
-    print("Results saved to '{}' successfully!".format(filename))
-
-
-def main():
-    """
-    To loop through each class file
-    """
-    file = input("Input your filename: ")
-    if check_file(file):
-        assignments = analyze(file)
-        scores = grade(assignments)
-        write_file(scores, file)
+	print("\n" + "EXPORTING RESULTS!".center(30, "-") + "\n")
+	filename = filename.replace(".txt", "_grades.txt")
+	with open(filename, "w+") as file_object:
+		for key, value in scores_dict.items():
+			file_object.write(key + "," + str(value) + "\n")
+	print("Results saved to '{}' successfully!".format(filename))
 
 
 while True:
-    main()
-    print("\n" + "RESTARTING!".center(30, "-") + "\n")
+	file = input("Input your filename: ")
+	if check_file(file):
+		assignments = analyze(file)
+		if assignments:
+			scores = grade(assignments)
+			write_file(scores, file)
+		else:
+			print("No valid line to grade!")
+	print("\n" + "RESTARTING!".center(30, "-") + "\n")
